ec5bc64c8e877f9759c76020259dc0333e2d111c
Hibnu Hishath <hibnuhishath1@gmail.com>
2022-03-10 21:24:33 -0800

    Bug fix for singleton class + addition of network singleton

--- Core/Singletons/NetworkSingleton.cs
+++ Core/Singletons/NetworkSingleton.cs
@@ -2,36 +2,39 @@ using UnityEngine;
 
 namespace Hibzz.Core.Singletons
 {
-    public class Singleton<T> : Monobehaviour where T : Component
+    public class NetworkSingleton<T> : NetworkBehaviour where T : Component
     {
         private static T instance;
         public static T Instance
         {
-            if(instance == null)
+            get
             {
-                instance = createNewInstance();
-            }
+                if (instance == null)
+                {
+                    instance = createNewInstance();
+                }
 
-            return instance;
+                return instance;
+            }
         }
 
         private static T createNewInstance()
         {
             T[] items = FindObjectsOfType(typeof(T)) as T[];
-            if(items.Length == 0)
+            if (items.Length == 0)
             {
                 // no instance of the singleton found in the scene... so creating one
                 GameObject obj = new GameObject();
-                obj.name = typeof(T).name + "Object";
+                obj.name = typeof(T).Name + "Object";
                 return obj.AddComponent<T>();
             }
-            else if(items.Length > 1)
+            else if (items.Length > 1)
             {
                 // more than one instance found. So returning null
                 Debug.LogError("Multiple instances of the singleton found. So, cant determine what's the singleton. Returning null.");
                 return null;
             }
-            
+
             // only one instance of type T found in the scene
             return items[0];
         }
